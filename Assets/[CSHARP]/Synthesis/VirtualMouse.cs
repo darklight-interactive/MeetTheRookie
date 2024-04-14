d34a5e497e700103e856c107ca80fdd2c88780da
Tyler K <tyly04@gmail.com>
2024-04-13 17:24:18 -0700

    Trying to simplify things with an interface

--- Assets/[CSHARP]/Synthesis/VirtualMouse.cs
+++ Assets/[CSHARP]/Synthesis/VirtualMouse.cs
@@ -7,9 +7,29 @@ using UnityEngine.InputSystem;
 using UnityEngine.PlayerLoop;
 using UnityEngine.UIElements;
 
-public class VirtualMouse : MonoBehaviour, ISceneSingleton<VirtualMouse>
+public class VirtualMouse : MonoBehaviour, ISceneSingleton<VirtualMouse>, IPointerEvent
 {
-    public Vector2 position { get; protected set; }
+    public Vector3 position { get; protected set; }
+
+    public int pointerId => 2;
+
+    public string pointerType => "Virtual";
+
+    public bool isPrimary => false;
+
+    public int button => -1;
+
+    public int pressedButtons => 0;
+
+    Vector3 IPointerEvent.position => position;
+
+    public Vector3 localPosition => position;
+
+    public Vector3 deltaPosition {get; protected set; }
+
+    public float deltaTime { get; protected set; }
+
+    public int clickCount => 0;
 
     protected void Awake() {
         (this as ISceneSingleton<VirtualMouse>).Initialize();
@@ -28,6 +48,14 @@ public class VirtualMouse : MonoBehaviour, ISceneSingleton<VirtualMouse>
         UniversalInputManager.PrimaryInteractAction.performed += Click;
         UniversalInputManager.PrimaryInteractAction.canceled += StopClick;
 
+        /*var e = new Event();
+        e.type = EventType.MouseEnterWindow;
+        e.mousePosition = position;
+        e.delta = position;
+        e.displayIndex = 0;
+        var mouseEvent = PointerEnterEvent.GetPooled(e);
+        mouseEvent.target = activeHook;
+        activeHook.SendEvent(mouseEvent);*/
     }
 
     Vector2 intendedMoveDir;
@@ -41,16 +69,14 @@ public class VirtualMouse : MonoBehaviour, ISceneSingleton<VirtualMouse>
     }
 
     /// <summary>
-    /// We need to send events during the event tick.
+    /// Can we send move events?
     /// </summary>
+    bool downSent = false;
     bool sendDown = false;
     void Click(InputAction.CallbackContext context) {
         sendDown = true;
     }
 
-    /// <summary>
-    /// Same with up events.
-    /// </summary>
     bool sendUp = false;
     void StopClick(InputAction.CallbackContext context) {
         sendUp = true;
@@ -58,53 +84,65 @@ public class VirtualMouse : MonoBehaviour, ISceneSingleton<VirtualMouse>
 
     VisualElement activeHook;
     void Update() {
-        Vector2 oldPos = position;
+        deltaTime = Time.deltaTime;
+
+        Vector3 oldPos = position;
         if (intendedMoveDir != Vector2.zero) {
-            position += intendedMoveDir;
-            position = new Vector2(Mathf.Clamp(position.x, 0, Screen.width), Mathf.Clamp(position.y, 0, Screen.height));
-
-            if (position - oldPos != Vector2.zero && activeHook != null) {
-                var e = new Event();
-                e.type = EventType.MouseMove;
-                e.mousePosition = position;
-                e.delta = position - oldPos;
-
-                // TODO: Why is this only detected on SynthesisDraggable when we move left?
-                var moveEvent = PointerMoveEvent.GetPooled(e);
-
-                activeHook.SendEvent(moveEvent);
-            }
+            position += new Vector3(intendedMoveDir.x, intendedMoveDir.y, 0);
+            position = new Vector3(Mathf.Clamp(position.x, 0, Screen.width), Mathf.Clamp(position.y, 0, Screen.height));
         }
-        if (sendDown) {
-            var e = new Event();
-            e.type = EventType.MouseDown;
-            e.mousePosition = position;
-            e.button = 0;
-            e.delta = Vector2.zero;
-
-            var mouseEvent = PointerDownEvent.GetPooled(e);
-            if (activeHook != null) {
-                activeHook.SendEvent(mouseEvent);
-            }
-            sendDown = false;
-        }
+        deltaPosition = position - oldPos;
 
         if (sendUp) {
-            var e = new Event();
-            e.type = EventType.MouseUp;
-            e.mousePosition = position;
-            e.delta = Vector2.zero;
-            e.button = 0;
-
-            var mouseEvent = PointerUpEvent.GetPooled(e);
-            if (activeHook != null) {
-                activeHook.SendEvent(mouseEvent);
-            }
+            downSent = false;
+            activeHook.SendEvent(PointerUpEvent.GetPooled(this));
             sendUp = false;
+        } else if (downSent) {
+            // TODO: Why does this only work when moving left or up?
+            activeHook.SendEvent(PointerMoveEvent.GetPooled(this));
+        }
+
+        if (sendDown) {
+            activeHook.SendEvent(PointerDownEvent.GetPooled(this));
+            downSent = true;
+            sendDown = false;
         }
     }
 
     public void HookTo(VisualElement toHook) {
         activeHook = toHook;
     }
+
+    #region Interface crap
+
+    public float pressure => 0;
+
+    public float tangentialPressure => 0;
+
+    public float altitudeAngle => 0;
+
+    public float azimuthAngle => 0;
+
+    public float twist => 0;
+
+    public Vector2 tilt => Vector2.zero;
+
+    public PenStatus penStatus => PenStatus.None;
+
+    public Vector2 radius => Vector2.zero;
+
+    public Vector2 radiusVariance => Vector2.zero;
+
+    public EventModifiers modifiers => EventModifiers.None;
+
+    public bool shiftKey => false;
+
+    public bool ctrlKey => false;
+
+    public bool commandKey => false;
+
+    public bool altKey => false;
+
+    public bool actionKey => false;
+    #endregion
 }
