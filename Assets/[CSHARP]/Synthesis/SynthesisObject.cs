89bc040da0eae390bfc550050adc53682a70af8b
Sky Casey <45044798+skysfalling@users.noreply.github.com>
2024-04-24 16:49:02 -0700

    22 synthesis keyboard control overhaul (#30)
    * Migrated over vector field
    
    * Add SynthesisManager to prefab
    
    * Remove cursor from synthesis
    
    * Add and remove selectables to MenuVectorField
    
    * Update synthesis.uss
    
    * Drafting selection setup
    
    * Selecting other synthesis items works
    
    * VectorField should be more generic
    
    * Selecting logic
    
    * Selection styling
    
    * Fixing selection bugs
    
    * Avoid locking up player during synthesis
    
    * Adding big synthesis button
    
    * Fixing move inverted bug
    
    * Allow deselecting and synthesizing
    
    * Selection highlight coloring
    
    * Getting ready to set up test file
    
    * Added synthesis example
    
    * Update SYNTHESIS_PROTOTYPE.unity
    
    ---------
    
    Co-authored-by: Tyler K <tyly04@gmail.com>

--- Assets/[CSHARP]/Synthesis/SynthesisObject.cs
+++ Assets/[CSHARP]/Synthesis/SynthesisObject.cs
@@ -8,13 +8,10 @@ public partial class SynthesisObject : VisualElement
 {
     public new class UxmlFactory : UxmlFactory<SynthesisObject> { }
 
-    SynthesisDraggable manipulator;
-
     public Label noteHeader;
     
     public SynthesisObject() {
         AddToClassList("synthesis-object");
-        manipulator = new SynthesisDraggable(this);
 
         noteHeader = new Label();
         noteHeader.AddToClassList("label");
