88de2bf24c37852962106fc635a825e0e5484656
~skysfall1ng~ <skythecreative@gmail.com>
2024-04-24 23:36:12 -0700

    tweaking the camera script

--- Assets/Thirdparty/Darklight/Game/CameraController/CameraRig3D.cs
+++ Assets/Thirdparty/Darklight/Game/CameraController/CameraRig3D.cs
@@ -5,26 +5,27 @@ using UnityEditor;
 
 namespace Darklight.Game.CameraController
 {
+    [ExecuteAlways]
     public class CameraRig3D : MonoBehaviour
     {
         #region [[ PRIVATE VARIABLES ]]
-        private Vector3 _cameraPosOffset => new Vector3(_xPosOffset, _yPosOffset, _zPosOffset);
-        private Transform _pivotHandle => this.transform; // Set to self transform
-        private Camera _camera;
+        private Camera _mainCamera;
+        private Camera[] _childCameras;
+
+        private Vector3 _cameraLocalPosOffset => new Vector3(_xPosOffset, _yPosOffset, _zPosOffset);
         #endregion
 
         #region [[ PUBLIC ACCESSORS ]]
         public bool Initialized { get; private set; } = false;
         #endregion
 
-        #region [[ PUBLIC INSPECTOR VARIABLES ]]
-        [Header("References")]
-        [SerializeField]
-        private GameObject cameraPrefab;
+        #region [[ Camera Settings ]]
+        [Header("Camera Target Offset")]
+        [SerializeField] private Transform _lookTarget;
+        [SerializeField] private Vector3 _lookTargetOffset = new Vector3(0, 0, 0);
 
-        public Transform focusTarget;
-
-        [Header("CameraPositionOffset"), SerializeField, Range(-10f, 10f)]
+        [Header("CameraPositionOffsets")]
+        [SerializeField, Range(-10f, 10f)]
         private float _xPosOffset = 0;
 
         [SerializeField, Range(0f, 10f)]
@@ -33,158 +34,126 @@ namespace Darklight.Game.CameraController
         [SerializeField, Range(-10f, 10f)]
         private float _zPosOffset = 0;
 
-        [Header("PivotHandleRotation"), SerializeField, Range(-180, 180)]
-        public float pivotHandleRotation = 0;
 
-        [Header("Speeds"), SerializeField, Range(0, 10f)]
-        private int followSpeed = 2;
 
         [SerializeField, Range(0, 10f)]
-        private int rotateSpeed = 2;
+        private int _followSpeed = 2;
 
-        public bool isPerspective = true; // Default to perspective mode
-        public float perspectiveFOV = 60f;
-        public float orthographicSize = 5f;
+        [SerializeField, Range(0, 10f)]
+        private int _rotateSpeed = 2;
+
+        [Header("Camera Projection")]
+        [SerializeField] private bool isPerspective = true; // Default to perspective mode
+        [SerializeField, Range(0, 100f)] private float perspectiveFOV = 20f;
+        [SerializeField] private float orthographicSize = 5f;
         #endregion
 
         // ========================================== //
         private void Awake()
         {
-            UpdateValues();
-        }
+            // Get all cameras in the hierarchy
+            _childCameras = GetComponentsInChildren<Camera>();
 
-        public void UpdateValues()
-        {
-            if (!_camera || !_pivotHandle)
+            if (_childCameras.Length > 0)
             {
-                CreateCamera();
+                _mainCamera = _childCameras[0];
             }
-
-            UpdateCameraProjection();
-            SetToEditorValues();
-            Initialized = true;
+            else
+            {
+                _mainCamera = CreateCameraChild();
+            }
         }
+
         public void ResetToDefaults()
         {
             _xPosOffset = 0;
             _yPosOffset = 5;
             _zPosOffset = -5;
-            pivotHandleRotation = 0;
-            followSpeed = 2;
-            rotateSpeed = 2;
+            _followSpeed = 2;
+            _rotateSpeed = 2;
 
             Initialized = false;
         }
 
-        void CreateCamera()
+        Camera CreateCameraChild()
         {
-            _camera = GetComponentInChildren<Camera>();
-            if (_camera == null)
-            {
-                // Create pivot handle at the root of the [SelectionBase]
-                _pivotHandle.localPosition = Vector3.zero;
+            Camera newCamera = new GameObject("default-CameraRig3D").AddComponent<Camera>();
+            newCamera.transform.parent = this.transform;
+            newCamera.transform.localPosition = Vector3.zero;
+            newCamera.transform.localRotation = Quaternion.identity;
 
-                // Prefab is set and no camera is found
-                if (cameraPrefab != null)
-                {
-                    _camera = Instantiate(cameraPrefab, _pivotHandle).GetComponent<Camera>(); // Create camera that is child of pivot
-                    _camera.transform.localPosition = Vector3.zero;
-                    _camera.transform.localRotation = Quaternion.identity;
-                }
-                else if (cameraPrefab == null)
-                {
-                    Debug.Log("Camera Prefab is not Assigned - Creating Default Camera");
-                    _camera = new GameObject("default-CameraRig3D").AddComponent<Camera>();
-                    _camera.transform.parent = _pivotHandle.transform;
-                    _camera.transform.localPosition = Vector3.zero;
-                    _camera.transform.localRotation = Quaternion.identity;
-                }
+            return newCamera;
+        }
+
+        void UpdateAllCamerasToEditorValues()
+        {
+            foreach (Camera cam in _childCameras)
+            {
+                cam.transform.localPosition = _mainCamera.transform.localPosition;
+                cam.transform.localRotation = _mainCamera.transform.localRotation;
             }
         }
 
         public void SetToEditorValues()
         {
-            if (!focusTarget || !_camera || !_pivotHandle.gameObject)
+            /*
+            if (!_mainCamera || !_pivotHandle.gameObject)
             {
-                Debug.Log($"focusTarget :: {focusTarget} _camera :: {_camera} :: _pivotHandle{_pivotHandle}");
                 return;
             }
 
-            // Calculate pivot rotation
-            _pivotHandle.rotation = GetPivotRotation();
-
             // Calculate and override position
-            _camera.transform.localPosition = GetCameraFollowPosition(
+            _mainCamera.transform.localPosition = GetCameraFollowPosition(
                 focusTarget.position,
-                _cameraPosOffset
+                _cameraLocalPosOffset
             );
 
             // Calculate and override rotation
-            _camera.transform.rotation = GetCameraLookRotation(
+            _mainCamera.transform.rotation = GetCameraLookRotation(
                 focusTarget.position
             );
-        }
-        // Method to switch to perspective mode
-        public void SwitchToPerspective()
-        {
-            isPerspective = true;
-            UpdateCameraProjection();
-        }
-
-        // Method to switch to orthographic mode
-        public void SwitchToOrthographic()
-        {
-            isPerspective = false;
-            UpdateCameraProjection();
+            */
         }
 
         // Update the camera's projection based on the current mode
-        private void UpdateCameraProjection()
+        private void UpdateCameraProjection(Camera cam)
         {
-            if (_camera != null)
+            if (_mainCamera != null)
             {
-                _camera.orthographic = !isPerspective;
+                _mainCamera.orthographic = !isPerspective;
 
                 if (isPerspective)
                 {
-                    _camera.fieldOfView = perspectiveFOV;
+                    _mainCamera.fieldOfView = perspectiveFOV;
                 }
                 else
                 {
-                    _camera.orthographicSize = orthographicSize;
+                    _mainCamera.orthographicSize = orthographicSize;
                 }
             }
         }
         public void Update()
         {
-            if (!focusTarget || !_camera || !_pivotHandle)
-            {
-                return;
-            }
+            if (_lookTarget == null) return;
 
-            Vector3 targetPosition = GetCameraFollowPosition
+            Vector3 lookTargetPosition = _lookTarget.position + _lookTargetOffset;
+
+            // << UPDATE CAMERA TO FOLLOW TARGET >>
+            Vector3 currentFollowPosition = GetCameraFollowPosition(lookTargetPosition, _cameraLocalPosOffset);
+            _mainCamera.transform.localPosition = Vector3.Lerp
             (
-                focusTarget.position,
-                _cameraPosOffset
-            );
-            _camera.transform.localPosition = Vector3.Lerp
-            (
-                _camera.transform.localPosition,
-                targetPosition,
-                followSpeed * Time.deltaTime
+                _mainCamera.transform.localPosition,
+                currentFollowPosition,
+                _followSpeed * Time.deltaTime
             );
 
             // Calculate and Slerp rotation
-            Quaternion targetRotation = GetCameraLookRotation(
-                focusTarget.position);
-            _camera.transform.localRotation = Quaternion.Slerp(
-                _camera.transform.localRotation,
+            Quaternion targetRotation = GetCameraLookRotation(lookTargetPosition);
+            _mainCamera.transform.localRotation = Quaternion.Slerp(
+                _mainCamera.transform.localRotation,
                 targetRotation,
-                rotateSpeed * Time.deltaTime
+                _rotateSpeed * Time.deltaTime
             );
-
-            // Update Pivot Rotation
-            _pivotHandle.rotation = GetPivotRotation();
         }
 
         #region << GETTER FUNCTIONS >>
@@ -196,15 +165,9 @@ namespace Darklight.Game.CameraController
 
         Quaternion GetCameraLookRotation(Vector3 focusTargetPosition)
         {
-            Vector3 direction = (focusTargetPosition - _camera.transform.position).normalized;
+            Vector3 direction = (focusTargetPosition - _mainCamera.transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(direction);
             return lookRotation;
-        }
-
-        public Quaternion GetPivotRotation()
-        {
-            Vector3 yAxisRotation = Vector3.up * pivotHandleRotation;
-            return Quaternion.Euler(yAxisRotation);
         }
         #endregion
     }
@@ -216,10 +179,7 @@ namespace Darklight.Game.CameraController
         CameraRig3D cameraScript;
         public void OnEnable()
         {
-            if (cameraScript != null)
-            {
-                cameraScript.UpdateValues();
-            }
+
         }
 
         public override void OnInspectorGUI()
@@ -230,6 +190,7 @@ namespace Darklight.Game.CameraController
             // Detect changes to the serialized properties
             EditorGUI.BeginChangeCheck();
 
+            /*
             if (GUILayout.Button("Set Values"))
             {
                 cameraScript.UpdateValues();
@@ -248,6 +209,7 @@ namespace Darklight.Game.CameraController
                 serializedObject.ApplyModifiedProperties();
                 cameraScript.UpdateValues();
             }
+            */
         }
     }
 #endif
