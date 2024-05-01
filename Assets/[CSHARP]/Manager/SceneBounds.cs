e78d9bdb37af4bcde44cbdad726320fa5b5fb977
tblagarrett <tblagarrett@gmail.com>
2024-04-30 17:45:23 -0700

    Created SceneBounds with gizmo

---
+++ Assets/[CSHARP]/Manager/SceneBounds.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneBounds : MonoBehaviour
+{
+    public float leftBound = -10f;
+    public float rightBound = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawLine(new Vector3(leftBound, -5, 10), new Vector3(leftBound, 5, 10));
+        Gizmos.DrawLine(new Vector3(rightBound, -5, 10), new Vector3(rightBound, 5, 10));
+    }
+}
