1a17fb2881fe66a070e9118ce46cde0c2968f389
~skysfall1ng~ <skythecreative@gmail.com>
2024-05-03 09:09:39 -0700

    psky clues

--- Assets/[CSHARP]/Interaction/Clue_Interactable.cs
+++ Assets/[CSHARP]/Interaction/Clue_Interactable.cs
@@ -9,25 +9,13 @@ public class Clue_Interactable : Interactable
 
     }
 
-    public UXML_WorldSpaceUI ShowDialogueBubble(string text)
-    {
-        OverlapGrid2D_Data data = this.GetBestData();
-        Vector3 position = data.worldPosition;
-
-        UXML_WorldSpaceUI worldSpaceUIDoc = UIManager.Instance.worldSpaceUI;
-        worldSpaceUIDoc.transform.position = position;
-        worldSpaceUIDoc.transform.localScale = data.coordinateSize * Vector3.one;
-        //worldSpaceUIDoc.SetText(text);
-        return worldSpaceUIDoc;
-    }
-
     protected override void Initialize()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 
     public override void OnDestroy()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 }
