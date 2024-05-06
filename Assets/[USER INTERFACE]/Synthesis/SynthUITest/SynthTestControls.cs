09ac8d1cbf7931f49ae89b962512895e6b6b9938
ethanurl <eearle@ucsc.edu>
2024-05-06 15:42:36 -0700

    Added a few things, test controls, etc.
    All Items have relevant public methods
    Select()
    Deselect()
    ChangeText()
    OnHover()
    
    Stack has other methods
    Change()
    TextChange()
    
    Do whatever you want with these!
    If you have any questions feel free to ask

--- Assets/[USER INTERFACE]/Synthesis/SynthUITest/SynthTestControls.cs
+++ Assets/[USER INTERFACE]/Synthesis/SynthUITest/SynthTestControls.cs
@@ -1,21 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SynthTestControls : MonoBehaviour
 {
-    /*
-    public List<UIDocument> SynthItems = new List<UIDocument>(); 
-    // Start is called before the first frame update
-    void Start()
+    public SynthesisItems[] SynthItems;
+    public SynthesisItems CurrentItem;
+    public SynthesisItems PreviousItem;
+    public int selector = 0;
+    public bool movable = true;
+    void OnEnable()
     {
-        foreach (UIDocument document in )
+        SynthItems = FindObjectsByType<SynthesisItems>(FindObjectsSortMode.InstanceID);
+        CurrentItem = SynthItems[selector];
+        CurrentItem.OnHover();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (movable == true)
+        {
+            if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+            {
+                Move("Left");
+            }
+            if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+            {
+                Move("Right");
+            }
+        }
     }
-    */
+    void Move(string dir)
+    {
+        PreviousItem = CurrentItem;
+        if (dir == "Left")
+        {
+            if (selector != 0)
+            {
+                selector -= 1;
+            } else {
+                selector = SynthItems.Length - 1;
+            }
+        }
+        if (dir == "Right")
+        {
+            if (selector != SynthItems.Length - 1)
+            {
+                selector += 1;
+            } else {
+                selector = 0;
+            }
+        }
+        CurrentItem = SynthItems[selector];
+        CurrentItem.OnHover();
+        PreviousItem.OnHover();
+        Debug.Log(CurrentItem.name);
+    }
 }
