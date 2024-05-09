f367953dff71f3c52c48e7dd638780a898f414ea
Tyler K <tyly04@gmail.com>
2024-05-08 19:23:25 -0700

    Profiling in debug and in editor

--- Assets/AnalyticsManager.cs
+++ Assets/AnalyticsManager.cs
@@ -4,14 +4,22 @@ using Unity.Services.Analytics;
 using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.Analytics;
+using UnityEngine.Profiling;
 
 public class AnalyticsManager : MonoBehaviourSingleton<AnalyticsManager> {
     // Start is called before the first frame update
     async void Start()
     {
+#if UNITY_EDITOR || DEBUG
+        Debug.Log("GAKSK");
+        Profiler.logFile = "profilerLog";
+        Profiler.enabled = true;
+        Profiler.enableBinaryLog = true;
+
         await UnityServices.InitializeAsync();
 
         AnalyticsService.Instance.StartDataCollection();
         PerformanceReporting.enabled = true;
+#endif
     }
 }
