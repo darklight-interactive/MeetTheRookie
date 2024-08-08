bb37419cdf71937963dc4b89cc3d4dcb2eda721a
Optimalystic <irkedibis@gmail.com>
2024-08-08 13:58:47 -0700

    Updated BackgroundMusicObject to change intensity on scene change, plus FMOD stuff
    Specifically, in FMOD I consolidated assets into less banks, hopefully making start time a little faster.
    Changes to scenes were adding FMOD listeners in 2_0 Dating Sim and updating the background music prefab in the 1_3 Gas Station

--- Assets/[CSHARP]/Manager/MTR_AudioManager.cs
+++ Assets/[CSHARP]/Manager/MTR_AudioManager.cs
@@ -9,6 +9,15 @@ public class MTR_AudioManager : FMOD_EventManager
 
     // Overwrite the generalSFX property to return the instance of the MTR_GeneralSFX class
     private new MTR_GeneralSFX generalSFX => base.generalSFX as MTR_GeneralSFX;
+    // Overwrite the backgroundMusic property to return the instance of the MTR_MusicObject class
+    public new MTR_MusicObject backgroundMusic => base.backgroundMusic as MTR_MusicObject;
+
+    public new void PlaySceneBackgroundMusic(string sceneName)
+    {
+        EventReference bg_music = backgroundMusic.GetBackgroundMusicByScene(sceneName);
+        backgroundMusic.SetMusicIntensity(sceneName);
+        PlaySong(bg_music);
+    }
 
     public void StartFootstepEvent()
     {
