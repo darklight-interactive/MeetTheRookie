63f83977084c5c9a4d044b46a0b3c6c92aee4a65
Optimalystic <irkedibis@gmail.com>
2024-09-16 17:56:06 -0700

    Merged with origin/build

--- Assets/[CSHARP]/Manager/MTR_AudioManager.cs
+++ Assets/[CSHARP]/Manager/MTR_AudioManager.cs
@@ -9,9 +9,9 @@ public class MTR_AudioManager : FMODExt_EventManager
     public static new MTR_AudioManager Instance => FMODExt_EventManager.Instance as MTR_AudioManager;
 
     // Overwrite the generalSFX property to return the instance of the MTR_GeneralSFX class
-    public new MTR_GeneralSFX generalSFX => base.generalSFX as MTR_GeneralSFX;
+    public new MTR_GeneralSFX generalSFX => base.GeneralSFX as MTR_GeneralSFX;
     // Overwrite the backgroundMusic property to return the instance of the MTR_MusicObject class
-    public new MTR_MusicObject backgroundMusic => base.backgroundMusic as MTR_MusicObject;
+    public new MTR_MusicObject backgroundMusic => base.BackgroundMusic as MTR_MusicObject;
 
     public new void PlaySceneBackgroundMusic(string sceneName)
     {
