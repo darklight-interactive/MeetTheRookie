eaa9207fdd4d162226ea567ba25f5fc92c24691d
~skysfall1ng~ <skythecreative@gmail.com>
2024-09-17 23:23:23 -0700

    move property drawer to seperate script

---
+++ Assets/Plugins/Darklight/_csharp/UnityExt/Library/Editor/LibraryPropertyDrawer.cs
@@ -0,0 +1,178 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+namespace Darklight.UnityExt.Library
+{
+    [CustomPropertyDrawer(typeof(Library<,>), true)]
+    public class LibraryPropertyDrawer : PropertyDrawer
+    {
+        private ReorderableList _list;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            SerializedProperty itemsProperty = property.FindPropertyRelative("_items");
+
+            // Initialize the ReorderableList if necessary
+            if (_list == null)
+            {
+                _list = new ReorderableList(property.serializedObject, itemsProperty, true, true, true, true);
+                _list.drawElementCallback = DrawElementCallback;
+                _list.drawHeaderCallback = DrawHeaderCallback;
+                _list.onAddCallback = OnAddCallback;
+                _list.onRemoveCallback = OnRemoveCallback;
+            }
+
+            // Update the serialized object and ReorderableList
+            _list.DoList(position);
+            //property.serializedObject.Update();
+            //property.serializedObject.ApplyModifiedProperties();
+
+            // Add some space at the bottom
+            EditorGUILayout.Space(EditorGUIUtility.singleLineHeight * 2);
+            EditorGUI.EndProperty();
+        }
+
+        private void DrawElementCallback(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty itemsProperty = _list.serializedProperty;
+            SerializedProperty itemProp = itemsProperty.GetArrayElementAtIndex(index);
+            SerializedProperty keyProp = itemProp.FindPropertyRelative("Key");
+            SerializedProperty valueProp = itemProp.FindPropertyRelative("Value");
+
+            rect.y += 2;
+            float halfWidth = rect.width / 2 - 5;
+
+            Rect keyRect = new Rect(rect.x, rect.y, halfWidth, EditorGUIUtility.singleLineHeight);
+            Rect valueRect = new Rect(rect.x + halfWidth + 10, rect.y, halfWidth, EditorGUIUtility.singleLineHeight);
+
+            EditorGUI.PropertyField(keyRect, keyProp, GUIContent.none);
+            EditorGUI.PropertyField(valueRect, valueProp, GUIContent.none);
+        }
+
+        private void DrawHeaderCallback(Rect rect)
+        {
+            float halfWidth = rect.width / 2 - 5;
+            EditorGUI.LabelField(new Rect(rect.x, rect.y, halfWidth, EditorGUIUtility.singleLineHeight), "Key");
+            EditorGUI.LabelField(new Rect(rect.x + halfWidth + 10, rect.y, halfWidth, EditorGUIUtility.singleLineHeight), "Value");
+        }
+
+        private void OnAddCallback(ReorderableList list)
+        {
+            SerializedProperty itemsProperty = list.serializedProperty;
+            itemsProperty.arraySize++;
+            list.index = itemsProperty.arraySize - 1;
+
+            SerializedProperty newItem = itemsProperty.GetArrayElementAtIndex(list.index);
+            SerializedProperty keyProp = newItem.FindPropertyRelative("Key");
+            SerializedProperty valueProp = newItem.FindPropertyRelative("Value");
+
+            // Initialize the new KeyValuePair with default values
+            InitializeProperty(keyProp);
+            InitializeProperty(valueProp);
+
+            itemsProperty.serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnRemoveCallback(ReorderableList list)
+        {
+            SerializedProperty itemsProperty = list.serializedProperty;
+            itemsProperty.DeleteArrayElementAtIndex(list.index);
+            itemsProperty.serializedObject.ApplyModifiedProperties();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (_list != null)
+            {
+                return _list.GetHeight();
+            }
+            return base.GetPropertyHeight(property, label);
+        }
+
+        private void InitializeProperty(SerializedProperty property)
+        {
+            switch (property.propertyType)
+            {
+                case SerializedPropertyType.Integer:
+                    property.intValue = default;
+                    break;
+                case SerializedPropertyType.Boolean:
+                    property.boolValue = default;
+                    break;
+                case SerializedPropertyType.Float:
+                    property.floatValue = default;
+                    break;
+                case SerializedPropertyType.String:
+                    property.stringValue = string.Empty;
+                    break;
+                case SerializedPropertyType.Color:
+                    property.colorValue = Color.white;
+                    break;
+                case SerializedPropertyType.ObjectReference:
+                    property.objectReferenceValue = null;
+                    break;
+                case SerializedPropertyType.LayerMask:
+                    property.intValue = default;
+                    break;
+                case SerializedPropertyType.Enum:
+                    property.enumValueIndex = 0;
+                    break;
+                case SerializedPropertyType.Vector2:
+                    property.vector2Value = Vector2.zero;
+                    break;
+                case SerializedPropertyType.Vector3:
+                    property.vector3Value = Vector3.zero;
+                    break;
+                case SerializedPropertyType.Vector4:
+                    property.vector4Value = Vector4.zero;
+                    break;
+                case SerializedPropertyType.Rect:
+                    property.rectValue = new Rect();
+                    break;
+                case SerializedPropertyType.ArraySize:
+                    property.intValue = default;
+                    break;
+                case SerializedPropertyType.Character:
+                    property.intValue = default;
+                    break;
+                case SerializedPropertyType.AnimationCurve:
+                    property.animationCurveValue = new AnimationCurve();
+                    break;
+                case SerializedPropertyType.Bounds:
+                    property.boundsValue = new Bounds();
+                    break;
+                case SerializedPropertyType.Gradient:
+                    // Gradient cannot be set directly
+                    break;
+                case SerializedPropertyType.Quaternion:
+                    property.quaternionValue = Quaternion.identity;
+                    break;
+                case SerializedPropertyType.ExposedReference:
+                    property.exposedReferenceValue = null;
+                    break;
+                case SerializedPropertyType.Vector2Int:
+                    property.vector2IntValue = Vector2Int.zero;
+                    break;
+                case SerializedPropertyType.Vector3Int:
+                    property.vector3IntValue = Vector3Int.zero;
+                    break;
+                case SerializedPropertyType.RectInt:
+                    property.rectIntValue = new RectInt();
+                    break;
+                case SerializedPropertyType.BoundsInt:
+                    property.boundsIntValue = new BoundsInt();
+                    break;
+                case SerializedPropertyType.ManagedReference:
+                    property.managedReferenceValue = null;
+                    break;
+                default:
+                    // For any other property types, set to default or null
+                    property.managedReferenceValue = null;
+                    break;
+            }
+        }
+    }
+}
--- Assets/Plugins/Darklight/_csharp/UnityExt/Library/EnumObjectLibrary.cs
+++ Assets/Plugins/Darklight/_csharp/UnityExt/Library/EnumObjectLibrary.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Darklight.UnityExt.Library
@@ -7,5 +10,24 @@ namespace Darklight.UnityExt.Library
         where TValue : UnityEngine.Object
     {
         public override TValue CreateDefaultValue() => default(TValue);
+
+        /// <summary>
+        /// Resets the library to contain all possible keys from the enum TKey,
+        /// with each value set to the default value of TValue.
+        /// </summary>
+        public void ResetLibrary()
+        {
+            // Get all possible values of the enum TKey
+            IEnumerable<TKey> enumValues = Enum.GetValues(typeof(TKey)).Cast<TKey>();
+
+            // Clear the current contents of the library
+            Clear();
+
+            // Add each enum value as a key with the default value for TValue
+            foreach (TKey key in enumValues)
+            {
+                Add(key, CreateDefaultValue());
+            }
+        }
     }
 }
